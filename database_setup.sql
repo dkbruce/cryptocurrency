11fc9f3174bffcefb89c897f26cdb0e0f6cc3ff8
dkbruce <dylankbruce@gmail.com>
2019-04-10 20:13:15 -0400

    ETL complete, sqlite database included

---
+++ database_setup.sql
@@ -0,0 +1,163 @@
+CREATE TABLE bitcoin(
+	`date` DATE,
+    `open` REAL,
+    high REAL,
+    low REAL,
+    `close` REAL,
+    volume BIGINT,
+    market_cap BIGINT,
+    PRIMARY KEY(`date`)
+);
+
+CREATE TABLE dash(
+	`date` DATE,
+    `open` REAL,
+    high REAL,
+    low REAL,
+    `close` REAL,
+    volume BIGINT,
+    market_cap BIGINT,
+    PRIMARY KEY(`date`)
+);
+
+CREATE TABLE ethereum(
+	`date` DATE,
+    `open` REAL,
+    high REAL,
+    low REAL,
+    `close` REAL,
+    volume BIGINT,
+    market_cap BIGINT,
+	PRIMARY KEY(`date`)
+);
+
+CREATE TABLE iota(
+	`date` DATE,
+    `open` REAL,
+    high REAL,
+    low REAL,
+    `close` REAL,
+    volume BIGINT,
+    market_cap BIGINT,
+    PRIMARY KEY(`date`)
+);
+
+CREATE TABLE litecoin(
+	`date` DATE,
+    `open` REAL,
+    high REAL,
+    low REAL,
+    `close` REAL,
+    volume BIGINT,
+    market_cap BIGINT,
+    PRIMARY KEY(`date`)
+);
+
+CREATE TABLE monero(
+	`date` DATE,
+    `open` REAL,
+    high REAL,
+    low REAL,
+    `close` REAL,
+    volume BIGINT,
+    market_cap BIGINT,
+    PRIMARY KEY(`date`)
+);
+
+CREATE TABLE neo(
+	`date` DATE,
+    `open` REAL,
+    high REAL,
+    low REAL,
+    `close` REAL,
+    volume BIGINT,
+    market_cap BIGINT,
+	PRIMARY KEY(`date`)
+);
+
+CREATE TABLE nem(
+	`date` DATE,
+    `open` REAL,
+    high REAL,
+    low REAL,
+    `close` REAL,
+    volume BIGINT,
+    market_cap BIGINT,
+    PRIMARY KEY(`date`)
+);
+
+CREATE TABLE numeraire(
+	`date` DATE,
+    `open` REAL,
+    high REAL,
+    low REAL,
+    `close` REAL,
+    volume BIGINT,
+    market_cap BIGINT,
+    PRIMARY KEY(`date`)
+);
+
+CREATE TABLE omisego(
+	`date` DATE,
+    `open` REAL,
+    high REAL,
+    low REAL,
+    `close` REAL,
+    volume BIGINT,
+    market_cap BIGINT,
+    PRIMARY KEY(`date`)
+);
+
+CREATE TABLE qtum(
+	`date` DATE,
+    `open` REAL,
+    high REAL,
+    low REAL,
+    `close` REAL,
+    volume BIGINT,
+    market_cap BIGINT,
+    PRIMARY KEY(`date`)
+);
+
+CREATE TABLE ripple(
+	`date` DATE,
+    `open` REAL,
+    high REAL,
+    low REAL,
+    `close` REAL,
+    volume BIGINT,
+    market_cap BIGINT,
+    PRIMARY KEY(`date`)
+);
+
+CREATE TABLE stratis(
+	`date` DATE,
+    `open` REAL,
+    high REAL,
+    low REAL,
+    `close` REAL,
+    volume BIGINT,
+    market_cap BIGINT,
+    PRIMARY KEY(`date`)
+);
+
+CREATE TABLE waves(
+	`date` DATE,
+    `open` REAL,
+    high REAL,
+    low REAL,
+    `close` REAL,
+    volume BIGINT,
+    market_cap BIGINT,
+    PRIMARY KEY(`date`)
+);
+
+CREATE TABLE sp(
+	`date` DATE,
+    `open` REAL,
+    high REAL,
+    low REAL,
+    `close` REAL,
+    volume BIGINT,
+    PRIMARY KEY(`date`)
+);
